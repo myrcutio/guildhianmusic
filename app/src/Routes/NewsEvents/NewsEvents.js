f41357dc5182f0a05618ddc14ad4d3e7763f2fc4
Ben Balentine <myrcutio@gmail.com>
2020-07-12 12:29:17 -0500

    add news and events content

--- app/src/Routes/NewsEvents/NewsEvents.js
+++ app/src/Routes/NewsEvents/NewsEvents.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import * as events from './events.json';
+
+const NewsEvent = (event) => (
+    <figure>
+        <figcaption>{event.title}</figcaption>
+        <figcaption>{event.subtitle}</figcaption>
+        {event.location ? (<span>in {event.location}</span>): ""}
+        {event.date ? (<span> {event.date}</span>): ""}
+        <p>{event.description}</p>
+        {event.postscript ? (<p>{event.postscript}</p>) : ""}
+    </figure>
+)
 
 export const NewsEvents = () => (
   <div>
     <Typography variant="h6">NewsEvents</Typography>
-    <p></p>
+      {events.events ? events.events.map(e => NewsEvent(e)) : ""}
   </div>
 );
