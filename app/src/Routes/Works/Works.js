abb91591d8342e3391752543caaef7f82d3fce04
Ben Balentine <myrcutio@gmail.com>
2020-07-12 11:41:40 -0500

    added works page content

--- app/src/Routes/Works/Works.js
+++ app/src/Routes/Works/Works.js
@@ -1,9 +1,26 @@
 import React from 'react';
 import Typography from '@material-ui/core/Typography';
+import * as works from './works.json';
+
+const Piece = (piece) => (
+    <section>
+        <div>{piece.title}</div>
+        <div>{piece.type}</div>
+        {piece.instruments ? piece.instruments.join(",") : "" }
+        <a href={piece.detailLink} target={piece.external ? "_blank" : ""}>Details</a>
+    </section>
+)
+
+const WorksCategory = (categoryName, category) => (
+    <figure className="category">
+        <figcaption>{categoryName}</figcaption>
+        {category ? category.map(c => Piece(c)) : ""}
+    </figure>
+);
 
 export const Works = () => (
   <div>
     <Typography variant="h6">Works</Typography>
-    <p></p>
+      {Object.keys(works.categories).map(category => WorksCategory(category, works.categories[category]))}
   </div>
 );
