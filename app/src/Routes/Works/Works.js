38e057b70eba30ae8fd8dd4cfc8155a4e0ed101d
Ben Balentine <myrcutio@gmail.com>
2020-07-12 17:18:28 -0500

    switched ternary to &&

--- app/src/Routes/Works/Works.js
+++ app/src/Routes/Works/Works.js
@@ -4,34 +4,34 @@ import * as works from './works.json';
 
 const Piece = (piece) => {
     const key = `${piece.title}-${piece.type}`
-    const instruments = piece.instruments ? piece.instruments.join(",") : ""
+    const instruments = piece.instruments && piece.instruments.join(",")
     return (
         <li key={key}>
             <div>{piece.title}</div>
             <div>{piece.type}</div>
-            {instruments ? (<div>{instruments}</div>) : null}
+            {instruments && (<div>{instruments}</div>)}
             <a href={piece.detailLink} target={piece.external ? "_blank" : ""}>Details</a>
         </li>
     )
 }
 
 const WorksCategory = (categoryName, category) => {
-    const pieces = category ? category.map(c => Piece(c)) : null
+    const pieces = category && category.map(c => Piece(c))
     return (
         <li className="category" key={categoryName}>
             <div>{categoryName}</div>
-            {pieces ? (<ul>{pieces}</ul>) : null}
+            {pieces && (<ul>{pieces}</ul>)}
         </li>
     );
 }
 
 export const Works = () => {
     const categoryKeys = Object.keys(works.categories)
-    const categories = categoryKeys ? categoryKeys.map(category => WorksCategory(category, works.categories[category])) : null
+    const categories = categoryKeys && categoryKeys.map(category => WorksCategory(category, works.categories[category]))
     return (
         <div>
             <Typography variant="h6">Works</Typography>
-            {categories ? (<ul>{categories}</ul>): null}
+            {categories && (<ul>{categories}</ul>)}
         </div>
     );
 }
