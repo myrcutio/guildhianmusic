4de0dbf778fbafec0526415a897ad72beb2b89b8
Ben Balentine <myrcutio@gmail.com>
2020-07-12 17:15:15 -0500

    split keys and maps into separate consts

--- app/src/Routes/Works/Works.js
+++ app/src/Routes/Works/Works.js
@@ -2,29 +2,36 @@ import React from 'react';
 import Typography from '@material-ui/core/Typography';
 import * as works from './works.json';
 
-const Piece = (piece) => (
-    <li key={piece.title + piece.type}>
-        <div>{piece.title}</div>
-        <div>{piece.type}</div>
-        {piece.instruments ? piece.instruments.join(",") : "" }
-        <a href={piece.detailLink} target={piece.external ? "_blank" : ""}>Details</a>
-    </li>
-)
+const Piece = (piece) => {
+    const key = `${piece.title}-${piece.type}`
+    const instruments = piece.instruments ? piece.instruments.join(",") : ""
+    return (
+        <li key={key}>
+            <div>{piece.title}</div>
+            <div>{piece.type}</div>
+            {instruments ? (<div>{instruments}</div>) : null}
+            <a href={piece.detailLink} target={piece.external ? "_blank" : ""}>Details</a>
+        </li>
+    )
+}
 
-const WorksCategory = (categoryName, category) => (
-    <li className="category" key={categoryName}>
-        <div>{categoryName}</div>
-        <ul>
-            {category ? category.map(c => Piece(c)) : ""}
-        </ul>
-    </li>
-);
+const WorksCategory = (categoryName, category) => {
+    const pieces = category ? category.map(c => Piece(c)) : null
+    return (
+        <li className="category" key={categoryName}>
+            <div>{categoryName}</div>
+            {pieces ? (<ul>{pieces}</ul>) : null}
+        </li>
+    );
+}
 
-export const Works = () => (
-  <div>
-    <Typography variant="h6">Works</Typography>
-      <ul>
-        {Object.keys(works.categories).map(category => WorksCategory(category, works.categories[category]))}
-      </ul>
-  </div>
-);
+export const Works = () => {
+    const categoryKeys = Object.keys(works.categories)
+    const categories = categoryKeys ? categoryKeys.map(category => WorksCategory(category, works.categories[category])) : null
+    return (
+        <div>
+            <Typography variant="h6">Works</Typography>
+            {categories ? (<ul>{categories}</ul>): null}
+        </div>
+    );
+}
