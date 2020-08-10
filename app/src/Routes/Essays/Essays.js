b6d1df1b3f5bdba12057f96c8c3a55f7471eaf04
James Balentine <jbalenti@sbcglobal.net>
2020-08-10 13:36:53 -0500

    added essays route

---
+++ app/src/Routes/Essays/Essays.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Typography from '@material-ui/core/Typography';
+
+export const Essays = () => (
+  <div>
+    <Typography variant="h6">Essays</Typography>
+    <p></p>
+  </div>
+);
